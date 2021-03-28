505b847474d67345099ad1ed4ac434c0d1f88188
Chris <christopher-dags@hotmail.com>
2021-03-27 22:06:10 -0400

    Added port 8082 to properties file, Fixed type for bootstrap class, refactored uri in BeerInventoryController,

--- src/main/java/guru/sfg/beer/inventory/service/web/controllers/BeerInventoryController.java
+++ src/main/java/guru/sfg/beer/inventory/service/web/controllers/BeerInventoryController.java
@@ -24,10 +24,10 @@ public class BeerInventoryController {
     private final BeerInventoryRepository beerInventoryRepository;
     private final BeerInventoryMapper beerInventoryMapper;
 
-    @GetMapping("api/v1/beer/{beerId}/inventory")
+    @GetMapping("/api/v1/beer/{beerId}/inventory")
     List<BeerInventoryDto> listBeersById(@PathVariable UUID beerId){
         log.debug("Finding Inventory for beerId:" + beerId);
-
+//        System.out.println("Finding Inventory for beerId:" + beerId);
         return beerInventoryRepository.findAllByBeerId(beerId)
                 .stream()
                 .map(beerInventoryMapper::beerInventoryToBeerInventoryDto)
