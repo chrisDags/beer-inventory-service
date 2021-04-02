51bbd4f713ce03daec70088029ca59687a1b1c1d
Chris <christopher-dags@hotmail.com>
2021-04-02 13:29:42 -0400

    Configured services to use a local mysql database

--- target/generated-sources/annotations/guru/sfg/beer/inventory/service/web/mappers/BeerInventoryMapperImpl.java
+++ target/generated-sources/annotations/guru/sfg/beer/inventory/service/web/mappers/BeerInventoryMapperImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-04-01T17:12:21-0400",
+    date = "2021-04-02T12:07:45-0400",
     comments = "version: 1.3.0.Final, compiler: javac, environment: Java 11.0.2 (Oracle Corporation)"
 )
 @Component
