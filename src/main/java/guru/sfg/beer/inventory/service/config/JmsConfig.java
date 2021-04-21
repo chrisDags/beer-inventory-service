98de92b214404e1ccdd694ac62323cdcebe4df65
Chris <christopher-dags@hotmail.com>
2021-04-20 22:04:46 -0400

    Handle Allocation Requests - Closes #4

--- src/main/java/guru/sfg/beer/inventory/service/config/JmsConfig.java
+++ src/main/java/guru/sfg/beer/inventory/service/config/JmsConfig.java
@@ -14,6 +14,10 @@ import java.util.List;
 public class JmsConfig {
 
     public static final String NEW_INVENTORY_QUEUE = "new-inventory";
+    public static final String ALLOCATE_ORDER_QUEUE = "allocate-order";
+    public static final String ALLOCATE_ORDER_RESPONSE_QUEUE = "allocate-order-response";
+
+
 
     @Bean // Serialize message content to json
     public MessageConverter jacksonJmsMessageConverter(ObjectMapper objectMapper){
