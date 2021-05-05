582a20bca6cdcb3c57b30b23ab80da532d4480a3
Chris <christopher-dags@hotmail.com>
2021-05-05 14:37:16 -0400

    Added route config for Inventory Service. Closes #4

--- src/main/java/guru/sfg/beer/inventory/service/services/AllocationServiceImpl.java
+++ src/main/java/guru/sfg/beer/inventory/service/services/AllocationServiceImpl.java
@@ -73,12 +73,11 @@ public class AllocationServiceImpl implements AllocationService {
             } else if (inventory > 0) { //partial allocation
                 beerOrderLine.setQuantityAllocated(allocatedQty + inventory);
                 beerInventory.setQuantityOnHand(0);
-                beerInventoryRepository.delete(beerInventory);
             }
 
-//            if (beerInventory.getQuantityOnHand() == 0) {
-//                beerInventoryRepository.delete(beerInventory);
-//            }
+            if (beerInventory.getQuantityOnHand() == 0) {
+                beerInventoryRepository.delete(beerInventory);
+            }
         });
 
     }
